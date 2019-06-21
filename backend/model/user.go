df64e089592533e21cbfd41f50ba1b6826e42a1b
harm-matthias-harms <harm-matthias.harms@gmx.de>
2019-06-21 16:58:03 +0200

    user validation backend

--- backend/model/user.go
+++ backend/model/user.go
@@ -2,8 +2,13 @@ package model
 
 import (
 	"errors"
+	"regexp"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var (
+	emailRegex = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
 )
 
 // User is the type of a user
@@ -16,19 +21,46 @@ type User struct {
 
 // Register handles the full registration process for the handler
 func (user *User) Register() (err error) {
-	if err := user.validate(); err != nil {
+	if valid, err := user.validate(); !valid {
 		return err
 	}
 	return nil
 }
 
-func (user *User) validate() error {
-	if user.Username == "" {
-		return errors.New("no username provided")
-	} else if user.Email == "" {
-		return errors.New("no email provided")
-	} else if user.Password == "" {
-		return errors.New("no password provided")
+func (user *User) validate() (bool, error) {
+	if valid, err := user.validateUsername(); !valid {
+		return valid, err
+	} else if valid, err := user.validateEmail(); !valid{
+		return valid, err
+	} else if valid, err := user.validatePassword(); !valid {
+		return valid, err
 	}
-	return nil
+	return true, nil
+}
+
+func (user *User) validateUsername() (bool, error) {
+	if user.Username == "" {
+		return false, errors.New("no username provided")
+	} else if match, _ := regexp.MatchString("^[a-zA-Z-_1-9]+$", user.Username); !match {
+		return false, errors.New("username has wrong pattern")
+	}
+	return true, nil
+}
+
+func (user *User) validateEmail() (bool, error) {
+	if user.Email == "" {
+		return false, errors.New("no email address provided")
+	} else if match, _ := regexp.MatchString(emailRegex, user.Email); !match {
+		return false, errors.New("email has wrong pattern")
+	}
+	return true, nil
+}
+
+func (user *User) validatePassword() (bool, error) {
+	if user.Password == "" {
+		return false, errors.New("no password provided")
+	} else if match, _ := regexp.MatchString("^\\S+$", user.Password); !match {
+		return false, errors.New("password has whitespace")
+	}
+	return true, nil
 }
--- backend/model/user_test.go
+++ backend/model/user_test.go
@@ -1,2 +1,81 @@
 package model
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserRegister(t *testing.T) {
+	tests := []struct {
+		User User
+		Err bool
+	}{
+		{
+			User: User{Username: "testPerson", Email: "test@mail.com", Password: "123"},
+			Err: false,
+		},
+		{
+			User: User{Email: "test@mail.com", Password: "123"},
+			Err: true,
+		},
+	}
+	for _, tt := range tests {
+		err := tt.User.Register()
+		if tt.Err {
+			assert.Error(t, err)
+		}
+	}
+}
+
+func TestUserValidation(t *testing.T) {
+	tests := []struct {
+		User  User
+		Valid bool
+		Err   bool
+	}{
+		{
+			User:  User{Username: "testPerson", Email: "test@mail.com", Password: "123"},
+			Valid: true,
+			Err:   false,
+		},
+		{
+			User:  User{Email: "test@mail.com", Password: "123"},
+			Valid: false,
+			Err:   true,
+		},
+		{
+			User:  User{Username: "test Person", Email: "test@mail.com", Password: "123"},
+			Valid: false,
+			Err:   true,
+		},
+		{
+			User:  User{Username: "testPerson", Password: "123"},
+			Valid: false,
+			Err:   true,
+		},
+		{
+			User:  User{Username: "testPerson", Email: "testmail.com", Password: "123"},
+			Valid: false,
+			Err:   true,
+		},
+		{
+			User:  User{Username: "testPerson", Email: "test@mail.com"},
+			Valid: false,
+			Err:   true,
+		},
+		{
+			User:  User{Username: "testPerson", Email: "test@mail.com", Password: "1 23"},
+			Valid: false,
+			Err:   true,
+		},
+	}
+
+	for _, tt := range tests {
+		valid, err := tt.User.validate()
+		assert.Equal(t, tt.Valid, valid)
+		if tt.Err {
+			assert.Error(t, err)
+		}
+	}
+}
