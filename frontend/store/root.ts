fdaa61205e1c5f2c6c54970cf7ff8198c3f3b403
harm-matthias-harms <harm-matthias.harms@gmx.de>
2019-07-09 12:21:53 +0200

    init store

---
+++ frontend/store/root.ts
@@ -0,0 +1,15 @@
+import { GetterTree, ActionTree, MutationTree } from 'vuex'
+
+export const types = {}
+
+export interface State {}
+
+export const state = (): State => ({})
+
+export const getters: GetterTree<State, State> = {}
+
+export interface Actions<S, R> extends ActionTree<S, R> {}
+
+export const actions: Actions<State, State> = {}
+
+export const mutations: MutationTree<State> = {}
