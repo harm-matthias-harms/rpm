99dbad6d577f3b07fc512a183148d15b1da8762d
harm-matthias-harms <harm-matthias.harms@gmx.de>
2020-03-21 18:05:06 +0100

    fix linting

--- frontend/store/exercise/mutations.ts
+++ frontend/store/exercise/mutations.ts
@@ -2,10 +2,10 @@ import { MutationTree } from 'vuex'
 import { State } from './type'
 
 export const mutations: MutationTree<State> = {
-  SET_EXERCISE(state, exercise) {
+  SET_EXERCISE (state, exercise) {
     state.exercise = exercise
   },
-  UNSET_EXERCISE(state) {
+  UNSET_EXERCISE (state) {
     state.exercise = {
       id: undefined,
       author: {
