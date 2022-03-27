8caf78ec99f011250dcf7924a9e58b2c0e1d93ca
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2022-03-27 12:48:10 +0200

    Add clone functionality (#174)

--- frontend/pages/medical_cases/new.vue
+++ frontend/pages/medical_cases/new.vue
@@ -19,29 +19,49 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { mapActions } from 'vuex'
+import { mapActions, mapState } from 'vuex'
 import Form from '@/components/medical_case/form.vue'
+import { MedicalCase } from '~/store/medicalCase/type'
+import { defaultMedicalCase } from '~/store/medicalCase/state'
 
 @Component({
   components: {
-    Form
+    Form,
+  },
+  computed: {
+    ...mapState('medicalCase', {
+      clone: 'medicalCase',
+    }),
   },
   methods: {
     ...mapActions('medicalCase', {
-      create: 'create'
-    })
-  }
+      create: 'create',
+      find: 'find',
+    }),
+  },
 })
 export default class NewMedicalCase extends Vue {
-  medicalCase = {
-    general: {},
-    patient: {},
-    medical: {},
-    makeup: {},
-    vitalSigns: [],
-    files: []
-  }
-
+  medicalCase: MedicalCase = Object.assign({}, defaultMedicalCase)
   create!: (medicalCase, files) => void
+  find!: (id) => void
+
+  async mounted() {
+    const cloneId = this.$route.query.cloneId
+    if (cloneId) {
+      let clone: MedicalCase = await this.$axios.$get(
+        `/api/medical_cases/${cloneId}`
+      )
+      clone = {
+        ...clone,
+        id: undefined,
+        author: {},
+        editor: {},
+        createdAt: undefined,
+        editedAt: undefined,
+        files: [],
+      }
+      this.medicalCase = Object.assign({}, clone)
+    }
+  }
 }
 </script>
